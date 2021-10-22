89f86b7335d0b04a732621253f32c3439c57ab61
Jivendra Kumar <49364310+jivendra123@users.noreply.github.com>
2021-10-22 10:52:38 +0530

    Add files via upload

---
+++ src/main/java/com/example/service1/service1/Service1Controller.java
@@ -0,0 +1,20 @@
+package com.example.service1.service1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/service1")
+public class Service1Controller {
+	
+	//@GetMapping("/get")
+	public String test1() {
+		return "test1 method of service1";
+	}
+	//@GetMapping("/get")
+	public String test2() {
+		return "test1 method of service1";
+	}
+
+}
